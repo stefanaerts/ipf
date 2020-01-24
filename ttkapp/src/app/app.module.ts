a2b7935677d418afcc78576ddac313e4950de6e1
aertsst <Stefan.AERTS@ext.ec.europa.eu>
2020-01-24 16:44:43 +0100

    added hosting on firebase

--- ttkapp/src/app/app.module.ts
+++ ttkapp/src/app/app.module.ts
@@ -14,7 +14,8 @@ import { environment } from '../environments/environment';
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule,
+    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })],
   providers: [
     StatusBar,
     SplashScreen,
@@ -22,4 +23,4 @@ import { environment } from '../environments/environment';
   ],
   bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }
